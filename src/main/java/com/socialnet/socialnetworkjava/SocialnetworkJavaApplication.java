049411df421bda1f866fa798dc41ef2b6c677a98
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-11-19 19:54:28 +0300

    Initial commit
    Initial commit
    
    Создал проект Java Spring

---
+++ src/main/java/com/socialnet/socialnetworkjava/SocialnetworkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.socialnet.socialnetworkjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialnet/socialnetworkjava/SocialnetworkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnet.socialnetworkjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
